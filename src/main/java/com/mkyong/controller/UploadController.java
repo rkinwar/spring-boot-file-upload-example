f029a2b27828e9d918de06bf05885df019ba28c5
rkinwar <rkinwar@deloitte.ca>
2019-03-25 10:42:48 -0400

    Added port and changed file upload location

--- src/main/java/com/mkyong/controller/UploadController.java
+++ src/main/java/com/mkyong/controller/UploadController.java
@@ -16,7 +16,7 @@ import java.nio.file.Paths;
 public class UploadController {
 
     //Save the uploaded file to this folder
-    private static String UPLOADED_FOLDER = "F://temp//";
+    private static String UPLOADED_FOLDER = "//Users//rkinwar//digitalacademy//";
 
     @GetMapping("/")
     public String index() {
